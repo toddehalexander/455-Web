2a50aff5ee47b64c84ce374b0b84a25c54dc4af7
Toddeh Alexander <alexandertoddeh@gmail.com>
2024-03-28 20:16:49 -0700

    folder create

--- inClass_Mar.28.24/codesamples_04/defenses/escaping/app.js
+++
@@ -1,6 +0,0 @@
-const expressSanitizer = require('express-sanitizer');
-
-  var escaped = htmlEscape('"><script>alert(\'pwn\')</script>');
-
-https://www.npmjs.com/package/express-sanitizer
-console.log(escaped);
